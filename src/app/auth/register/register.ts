0dee451c22f2fb926af7ba8ce40d25ea7b20bc8b
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-09-29 10:19:38 +0300

    egister.ts

---
+++ src/app/auth/register/register.ts
@@ -0,0 +1,56 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
+  templateUrl: './register.html',
+  styleUrl: './register.scss'
+})
+export class Register {
+
+    private fb = inject(FormBuilder);
+    private authService = inject(AuthService);
+    private router = inject(Router);
+
+    isLoading = false;
+    error_message = '';
+    successMessage = '';
+
+    registerForm = this.fb.group({
+        firstname: ['',Validators.required],
+        lastname: ['',Validators.required],
+        email: ['', Validators.required,Validators.email],
+        username: ['',Validators.required],
+        password: ['',Validators.required],
+        couse: ['', Validators.required]
+    });
+
+    onSubmit(){
+      if(this.registerForm.valid){
+        this.isLoading = true;
+        this.error_message = '';
+        this.successMessage = '';
+
+        this.authService.register(this.registerForm.value).subscribe({
+          next: (response) => {
+            this.isLoading = false;
+            this.successMessage = response.message;
+
+            this.router.navigate(['/verify-otp'],{
+              queryParams: {email:this.registerForm.value.email}
+            });
+          },
+          error: (error) => {
+            this.isLoading = false;
+            this.error_message = error.error?.message || 'Registration Failed. Please try again.';
+          }
+        });
+      }
+    }
+
+}
