1a9f91f1909fdf909064b3685de751b3f9bc998a
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-09-29 11:25:26 +0300

    app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'/dashboard',
+        pathMatch:'full'
+    },
+    {
+        path:'register',
+        loadComponent: () => import('./auth/register/register').then(c => c.Register)
+    },
+    {
+        path:'login',
+        loadComponent: () => import('./auth/login/login').then(c => c.Login)
+    },
+    {
+        path:'verify-otp',
+        loadComponent: () => import('./auth/verify-otp/verify-otp').then(c => c.VerifyOtp)
+    },
+    {
+        path:'dashboad',
+        loadComponent: () => import('./auth/dashboard/dashboard').then(c => c.Dashboard)
+    }    
+];
