eea6f4acad44935198039fb3547a78c2a0b860f8
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-09-29 11:14:37 +0300

    dashboard ts

---
+++ src/app/auth/dashboard/dashboard.ts
@@ -0,0 +1,47 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { AuthService } from '../../core/services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone:true,
+  imports: [CommonModule],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.scss'
+})
+export class Dashboard implements OnInit {
+
+
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  user: User | null = null;
+
+  ngOnInit(): void {
+      this.authService.currentUser$.subscribe(user => {
+        this.user = user;
+      });
+
+      if(!this.user){
+        this.authService.getUserData().subscribe();
+      }
+  }
+
+  navigateToProfile(){
+    this.router.navigate(['/profile']);
+  }
+
+  logout(){
+    this.authService.logout().subscribe({
+      next: () => {
+        this.router.navigate(['/login']);
+      },
+      error: () => {
+        this.router.navigate(['/login']);
+      }
+    });
+  }
+
+
+}
