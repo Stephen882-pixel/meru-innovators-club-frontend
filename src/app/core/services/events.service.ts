05dce804b9277466a8960f2b14f9586abfb9f30b
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-10-02 20:24:24 +0300

    Event Interface

---
+++ src/app/core/services/events.service.ts
@@ -0,0 +1,16 @@
+
+
+export interface Event {
+  id:number;
+  name:string;
+  category:string;
+  title:string;
+  description:string;
+  date:string;
+  location:string;
+  organizer:string;
+  contact_email:string;
+  is_virtual:boolean;
+}
+
+
