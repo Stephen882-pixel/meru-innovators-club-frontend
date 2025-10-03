196dffe1c96b6ceb455405dce1b8413afa40a6a4
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-10-03 14:24:39 +0300

    event Form

---
+++ src/app/event/event-form/event-form.ts
@@ -0,0 +1,42 @@
+import {Component, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {ActivatedRoute, Router, RouterLink} from '@angular/router';
+import {EventsService} from '../../core/services/events.service';
+import {AuthService} from '../../core/services/auth.service';
+
+@Component({
+  selector: 'app-event-form',
+  standalone:true,
+  imports: [CommonModule,ReactiveFormsModule,RouterLink],
+  templateUrl: './event-form.html',
+  styleUrls: ['./event-form.scss']
+})
+export class EventForm {
+
+  private fb = inject(FormBuilder);
+  private eventService = inject(EventsService);
+  private authService = inject(AuthService);
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+
+  isEditMode = false;
+  eventId: number | null = null;
+  isSubmitting = false;
+  errorMessage = '';
+  successMessage = '';
+
+  eventForm = this.fb.group({
+    name:['',Validators.required],
+    category:['',Validators.required],
+    title:['',Validators.required],
+    description:['',Validators.required],
+    date:['',Validators.required],
+    location:['',Validators.required],
+    organizer:['',Validators.required],
+    contact_email: ['', [Validators.required, Validators.email]],
+    is_virtual:[false]
+  });
+
+
+}
