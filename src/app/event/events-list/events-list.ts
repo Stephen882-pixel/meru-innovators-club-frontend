8da336e96ba8e7eb0e755159002012b289ce1b1f
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-10-03 09:38:32 +0300

    formatDate method

--- src/app/event/events-list/events-list.ts
+++ src/app/event/events-list/events-list.ts
@@ -99,6 +99,17 @@ export class EventsList implements  OnInit{
     }
   }
 
+  formatDate(dateString:string):string{
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US',{
+      year: 'numeric',
+      month:'long',
+      day:'numeric',
+      hour:'2-digit',
+      minute:'2-digit'
+    });
+  }
+
 
 
 }
