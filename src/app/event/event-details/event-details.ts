34ae46b746a0a1291b907271c8af9c5f7f9e061f
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-10-04 11:21:16 +0300

    made changes to html

--- src/app/event/event-details/event-details.ts
+++ src/app/event/event-details/event-details.ts
@@ -70,7 +70,7 @@ export class EventDetails implements  OnInit{
 
   editEvent(){
     if(this.event){
-      this.router.navigate(['/events',this.event.id,'edit']);
+      this.router.navigate(['/events','edit']);
     }
   }
 
