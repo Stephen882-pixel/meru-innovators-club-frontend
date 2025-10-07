653b9abed86921344d8e534f88adf9fa9edead8d
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-10-07 12:04:47 +0300

    ngOnInit method

---
+++ src/app/executives/executive-detail/executive-detail.ts
@@ -0,0 +1,28 @@
+import {Component, inject, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {CommunitiesService, Executive} from '../../core/services/communities.service';
+import {ActivatedRoute, Router} from '@angular/router';
+
+@Component({
+  selector: 'app-executive-detail',
+  standalone:true,
+  imports: [CommonModule],
+  templateUrl: './executive-detail.html',
+  styleUrls:['./executive-detail.scss']
+})
+export class ExecutiveDetail implements OnInit{
+  private communitiesService = inject(CommunitiesService);
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+
+  executive: Executive | null = null;
+  isLoading = true;
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      const executiveId = +params['id'];
+      this.loadExecutiveDetails(executiveId);
+    });
+  }
+
+}
